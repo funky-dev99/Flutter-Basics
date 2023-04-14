ae66b4a7789d6d370a44f2eed5e0af601c9a0257
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-04-15 02:20:03 +0530

    Dicee

--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -1,53 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(App());
-
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+void main () {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    )
+  );
+}
+class DicePage extends StatelessWidget {
+  const DicePage ({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MainPage(),
-    );
-  }
-}
-class MainPage extends StatefulWidget {
-  const MainPage({Key? key}) : super(key: key);
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey[50],
-      appBar: AppBar(
-        backgroundColor: Colors.amberAccent,
-        title: const Text("I am Poor"),
-      ) ,
-
-      body: const Center(
-        child: Image(
-            image:AssetImage('images/poor.png')),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.amber,
-        child: Icon(Icons.add),
-        onPressed: () {  },
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: MaterialButton(
+                  onPressed: (){
+                    print('Fuck');
+                  },
+                child: Image.asset('images/poor.png'),
+              ),
+          ),
+          Expanded(
+            child: MaterialButton(
+              onPressed: (){},
+              child: Image.asset('images/poor.png'),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
-
-
-
-
-
-
-    
-    
