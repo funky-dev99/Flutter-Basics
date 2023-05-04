969694347bfbc0fd29ac19fb5b46e01f766d2e15
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-05-04 13:41:46 +0530

    ListView

--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -1,46 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main () {
-  return runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('Shahiru',style: TextStyle(
-            fontSize: 17.0, color: Colors.black
-          ),),
-          backgroundColor: Colors.blue,
-        ),
-        body: DicePage(),
-      ),
-    )
-  );
-}
-class DicePage extends StatelessWidget {
-  const DicePage ({Key? key}) : super(key: key);
+void main() => runApp(
+  DevicePreview(
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+);
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String _title = 'Flutter Code Sample';
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-              child: MaterialButton(
-                  onPressed: (){
-                    // print('Fuck');
-                  },
-                child: Image.asset('images/poor.png'),
-              ),
-          ),
-          Expanded(
-            child: MaterialButton(
-              onPressed: (){},
-              child: Image.asset('images/poor.png'),
-            ),
-          ),
-        ],
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  // int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text('Demo App'),
       ),
+      body: ListView.separated(
+        itemBuilder: (context, position) {
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                'List Item $position',
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (context, position) {
+          return Card(
+            color: Colors.grey,
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Text(
+                'Separator $position',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          );
+        },
+        itemCount: 20,
+      ),
+
+
+      // body: Center(child: Text('You have pressed the button $_count times.')),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () => setState(() => _count++),
+      //   tooltip: 'Increment Counter',
+      //   child: const Icon(Icons.add),
+      // ),
     );
   }
 }
