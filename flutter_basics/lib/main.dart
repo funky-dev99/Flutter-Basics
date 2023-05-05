6b0c63434f6febc2b190946d8b73ae0328d13fc9
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-05-05 23:50:54 +0530

    Bottom Nav Bar

--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -31,57 +31,27 @@ class MyStatefulWidget extends StatefulWidget {
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  // int _count = 0;
+   int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:  Text('Demo App'),
+      backgroundColor: Colors.blueGrey.shade200,
+       body: Center(
+        child: Text('Hello', style: TextStyle(fontSize: 30),),
       ),
-      body: Form
-        (child:SingleChildScrollView(
-          child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(
-                icon: const Icon(Icons.person),
-                hintText: 'Enter your name',
-                labelText: 'Name',
-              ),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                icon: const Icon(Icons.phone),
-                hintText: 'Enter a phone number',
-                labelText: 'Phone',
-              ),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                icon: const Icon(Icons.calendar_today),
-                hintText: 'Enter your date of birth',
-                labelText: 'Dob',
-              ),
-            ),
-            new Container(
-                padding: const EdgeInsets.only(left: 130.0, top: 40.0),
-                child: ElevatedButton(
-                  child: const Text('Submit'),
-                  onPressed: null,
-                )),
-          ],
-      ),
-        ),
-      )
+      bottomNavigationBar:
+      BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.message),label: 'Message'),
+          BottomNavigationBarItem(icon: Icon(Icons.contacts_sharp),label: 'Contact'),
+          BottomNavigationBarItem(icon: Icon(Icons.camera_alt),label: 'Camera'),
+    ],
+        backgroundColor: Colors.grey,
+        currentIndex: _selectedIndex,
+    ),
 
-      // body: Center(child: Text('You have pressed the button $_count times.')),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () => setState(() => _count++),
-      //   tooltip: 'Increment Counter',
-      //   child: const Icon(Icons.add),
-      // ),
     );
   }
 }
