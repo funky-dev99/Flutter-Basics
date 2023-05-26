b86248fa1ccc4c87bb7f95916e4e655cccdb1e1e
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-05-26 14:37:06 +0530

    Material Buttons

--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -1,134 +1,92 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basics/attendance.dart';
 
-
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  final List<String> containerTexts = [
-    'Container 0',
-    'Container 1',
-    'Container 2',
-    'Container 3',
-    'Container 4',
-    'Container 5',
-  ];
-
-  final List<String> imagePaths = [
-    'assets/images/image0.png',
-    'assets/images/image1.png',
-    'assets/images/image2.png',
-    'assets/images/image3.png',
-    'assets/images/image4.png',
-    'assets/images/image5.png',
-  ];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Button Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter GridView'),
+          title: Text('Button Example'),
         ),
-        body: Container(
-          padding: EdgeInsets.all(20.0),
-          child: GridView.count(
-            crossAxisCount: 2,
-            children: List.generate(6, (index) {
-              return _buildContainer(index, context); // Pass the context to _buildContainer
-            }),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildContainer(int index, BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10.0),
-      child: SizedBox(
-        width: 145.0,
-        height: 145.0,
-        child: MaterialButton(
-          onPressed: () {
-            // Replace with your own logic for each container
-            switch (index) {
-              case 0:
-                _handleContainer0Pressed(context); // Pass the context to _handleContainerXPressed methods
-                break;
-              case 1:
-                _handleContainer1Pressed(context);
-                break;
-              case 2:
-                _handleContainer2Pressed(context);
-                break;
-              case 3:
-                _handleContainer3Pressed(context);
-                break;
-              case 4:
-                _handleContainer4Pressed(context);
-                break;
-              case 5:
-                _handleContainer5Pressed(context);
-                break;
-            }
-          },
+        body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset(
-                imagePaths[index],
-                width: 80.0,
-                height: 80.0,
+              MaterialButton(
+                onPressed: () {
+                  // Add your logic here for the first button
+                },
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'images/loc.png',
+                      height: 110,
+                      width: 170,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Mark With Location',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+                padding: EdgeInsets.all(10),
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: BorderSide(
+                    color: Colors.grey, // Gray border color
+                    width: 2, // Border width
+                  ),
+                ),
+                minWidth: 215,
+                height: 170,
+                color: Colors.white, // White background color
               ),
-              SizedBox(height: 10.0),
-              Text(
-                containerTexts[index],
-                style: TextStyle(fontSize: 18.0),
+              SizedBox(height: 20),
+              MaterialButton(
+                onPressed: () {
+                  // Add your logic here for the second button
+                },
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/second_image.jpg',
+                      height: 110,
+                      width: 170,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Second Button',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+                padding: EdgeInsets.all(0),
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: BorderSide(
+                    color: Colors.grey, // Gray border color
+                    width: 2, // Border width
+                  ),
+                ),
+                minWidth: 215,
+                height: 170,
+                color: Colors.white, // White background color
               ),
             ],
           ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          elevation: 2.0,
-          color: Colors.white,
-          textColor: Colors.black,
         ),
       ),
     );
-  }
-
-  // Separate onPressed functions for each container
-  void _handleContainer0Pressed(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => Attendance()), // Replace NextPage with the desired page widget
-    );
-  }
-
-  void _handleContainer1Pressed(BuildContext context) {
-    // Action for Container 1
-    print('Container 1 pressed!');
-  }
-
-  void _handleContainer2Pressed(BuildContext context) {
-    // Action for Container 2
-    print('Container 2 pressed!');
-  }
-
-  void _handleContainer3Pressed(BuildContext context) {
-    // Action for Container 3
-    print('Container 3 pressed!');
-  }
-
-  void _handleContainer4Pressed(BuildContext context) {
-    // Action for Container 4
-    print('Container 4 pressed!');
-  }
-
-  void _handleContainer5Pressed(BuildContext context) {
-    // Action for Container 5
-    print('Container 5 pressed!');
   }
 }
