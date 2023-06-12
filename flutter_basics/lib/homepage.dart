628e555652dc0a8bdb336838d1f43c1c526565a6
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-06-12 15:03:13 +0530

    Add to Home Page

--- flutter_basics/lib/homepage.dart
+++ flutter_basics/lib/homepage.dart
@@ -1,27 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:profile/profile.dart';
 
-class AdminPage extends StatefulWidget {
-  const AdminPage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  @override
-  State<AdminPage> createState() => _AdminPageState();
-}
-
-class _AdminPageState extends State<AdminPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
         body: Center(
-      child: Profile(
-        imageUrl:
+          child: Profile(
+            imageUrl:
             "https://images.unsplash.com/photo-1598618356794-eb1720430eb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-        name: "Shamim Miah",
-        website: "shamimmiah.com",
-        designation: "Project Manager | Flutter & Blockchain Developer",
-        email: "cse.shamimosmanpailot@gmail.com",
-        phone_number: "01757736053",
-      ),
-    ));
+            name: "Shamim Miah",
+            website: "shamimmiah.com",
+            designation: "Project Manager | Flutter & Blockchain Developer",
+            email: "cse.shamimosmanpailot@gmail.com",
+            phone_number: "01757736053",
+          ),
+        )
+    );
   }
 }
